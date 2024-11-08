46ddaf367c563ca86fd4c80a73c614c91457ee29
younggeun <chldudrms94@naver.com>
2024-11-08 16:25:30 +0900

    add User Entity

---
+++ src/main/java/com/bestcat/delivery/user/entity/User.java
@@ -0,0 +1,40 @@
+package com.bestcat.delivery.user.entity;
+
+import com.bestcat.delivery.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "p_user")
+public class User extends BaseEntity {
+    @Id
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RoleType role;
+
+    private boolean isPublic;
+
+}
