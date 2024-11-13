f2efc6377a7c636558b62d158c55089a13ebbff0
laira2 <laira2@gachon.ac.kr>
2024-11-14 03:23:59 +0900

    category 조회, 추가, 수정

---
+++ src/main/java/com/bestcat/delivery/category/service/CategoryService.java
@@ -0,0 +1,54 @@
+package com.bestcat.delivery.category.service;
+
+import com.bestcat.delivery.area.dto.AreaResponseDto;
+import com.bestcat.delivery.area.entity.Area;
+import com.bestcat.delivery.category.dto.CategoryRequestDto;
+import com.bestcat.delivery.category.dto.CategoryResponseDto;
+import com.bestcat.delivery.category.entity.Category;
+import com.bestcat.delivery.category.repository.CategoryRepository;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public void save(Category newCategory) {
+        categoryRepository.save(newCategory);
+    }
+
+    public List<CategoryResponseDto> searchAreas(String categoryName, UUID categoryId) {
+        List<Category> categories;
+        if (categoryName != null && categoryId != null) {
+            categories = categoryRepository.findByCategoryNameAndCategoryId(categoryName, categoryId);
+        } else if (categoryName != null) {
+            categories = categoryRepository.findByCategoryName(categoryName);
+        } else if (categoryId != null) {
+            categories = categoryRepository.findByCategoryId(categoryId);
+        } else {
+            categories = categoryRepository.findAll();
+        }
+
+        return categories.stream()
+                .map(CategoryResponseDto::fromCategory)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void updateCategory(UUID categoryId, @Valid CategoryRequestDto categoryRequestDto) {
+        Category updateCategory = categoryRepository.findById(categoryId).orElse(null);
+        if (updateCategory != null) {
+            updateCategory.update(categoryRequestDto);
+        }
+    }
+}
---
+++ src/main/java/com/bestcat/delivery/category/web/CategoryController.java
@@ -0,0 +1,42 @@
+package com.bestcat.delivery.category.web;
+
+import com.bestcat.delivery.category.dto.CategoryRequestDto;
+import com.bestcat.delivery.category.dto.CategoryResponseDto;
+import com.bestcat.delivery.category.entity.Category;
+import com.bestcat.delivery.category.service.CategoryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping("/categories")
+    public void createCategory(@Valid @RequestBody CategoryRequestDto categoryRequestDto) {
+        Category newCategory = categoryRequestDto.toEntity();
+
+        categoryService.save(newCategory);
+
+    }
+
+    @GetMapping("/categories")
+    public List<CategoryResponseDto> searchCategory(
+            @RequestParam(required = false) String categoryName,
+            @RequestParam(required = false) UUID categoryId) {
+        return categoryService.searchAreas(categoryName, categoryId);
+    }
+
+    @PutMapping("/categories/{categoryId}")
+    public void updateCategory(@PathVariable UUID categoryId, @Valid @RequestBody CategoryRequestDto categoryRequestDto) {
+        categoryService.updateCategory(categoryId, categoryRequestDto);
+    }
+
+
+}
