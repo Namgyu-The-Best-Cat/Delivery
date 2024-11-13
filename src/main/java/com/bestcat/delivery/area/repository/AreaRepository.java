583c51a89b681e04b8646c68ff89f674078a4464
laira2 <76409433+laira2@users.noreply.github.com>
2024-11-13 17:25:46 +0900

    Update AreaRepository.java
    ID 타입 변경

--- src/main/java/com/bestcat/delivery/area/repository/AreaRepository.java
+++ src/main/java/com/bestcat/delivery/area/repository/AreaRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.UUID;
 
-public interface AreaRepository extends JpaRepository<Area, Integer> {
+public interface AreaRepository extends JpaRepository<Area, UUID> {
 
     List<Area> findByCity(String city);
 
