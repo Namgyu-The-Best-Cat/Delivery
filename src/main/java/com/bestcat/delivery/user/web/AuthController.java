62fbd110970f7ea25fdffc43788924cfef7680ec
younggeun <chldudrms94@naver.com>
2024-11-15 04:07:41 +0900

    Refactor : ResponseEntity에 와일드 카드로 변경

--- src/main/java/com/bestcat/delivery/user/web/AuthController.java
+++ src/main/java/com/bestcat/delivery/user/web/AuthController.java
@@ -5,9 +5,11 @@ import static com.bestcat.delivery.common.type.ResponseMessage.NICKNAME_CAN_USE;
 import static com.bestcat.delivery.common.type.ResponseMessage.USERNAME_CANT_USE;
 import static com.bestcat.delivery.common.type.ResponseMessage.USERNAME_CAN_USE;
 
+import com.bestcat.delivery.common.dto.ErrorResponse;
 import com.bestcat.delivery.common.dto.SuccessResponse;
 import com.bestcat.delivery.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,16 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final UserService userService;
 
-    @GetMapping("/check-nickname")
-    public ResponseEntity<SuccessResponse<String>> checkNickname(@RequestParam(name = "nickname") String nickname) {
+    @GetMapping(value = "/check-nickname", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> checkNickname(@RequestParam(name = "nickname") String nickname) {
         return !userService.checkNickname(nickname) ? ResponseEntity.ok().body(SuccessResponse.of(NICKNAME_CAN_USE)) :
-                ResponseEntity.badRequest().body(SuccessResponse.of("FAIL", NICKNAME_CANT_USE));
+                ResponseEntity.badRequest().body(ErrorResponse.builder().code("FAIL").message(NICKNAME_CANT_USE.getMessage()).build());
     }
 
-    @GetMapping("/check-username")
-    public ResponseEntity<SuccessResponse<String>> checkUsername(@RequestParam(name = "username") String username) {
+    @GetMapping(value = "/check-username", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> checkUsername(@RequestParam(name = "username") String username) {
         return !userService.checkUsername(username) ? ResponseEntity.ok().body(SuccessResponse.of(USERNAME_CAN_USE)) :
-                ResponseEntity.badRequest().body(SuccessResponse.of("FAIL", USERNAME_CANT_USE));
+                ResponseEntity.badRequest().body(ErrorResponse.builder().code("FAIL").message(USERNAME_CANT_USE.getMessage()).build());
     }
 
 }
